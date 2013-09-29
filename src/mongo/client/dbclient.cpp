301d79360a779b4b757a60659c802c2ab36cde9a
Andreas Nilsson <andreas.nilsson@10gen.com>
2013-09-29 08:51:44 -0400

    SERVER-10567 Handling rsHealthPoll/PRIMARY stepdown race

--- src/mongo/client/dbclient.cpp
+++ src/mongo/client/dbclient.cpp
@@ -912,7 +912,7 @@ namespace mongo {
 
 #ifdef MONGO_SSL
         if ( cmdLine.sslOnNormalPorts ) {
-            p->secure( sslManager() );
+            return p->secure( sslManager() );
         }
 #endif
 
