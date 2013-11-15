3bd007391ec7563eb5619626fd00585fe84a4980
Hari Khalsa <hkhalsa@10gen.com>
2013-11-15 13:17:47 -0500

    SERVER-10026 don't forget to log when count fails

--- src/mongo/db/ops/count.cpp
+++ src/mongo/db/ops/count.cpp
@@ -67,18 +67,49 @@ namespace mongo {
             limit  = -limit;
         }
 
-        OpSettings settings;
-        settings.setBulkFetch(true);
-        settings.setQueryCursorMode(DEFAULT_LOCK_CURSOR);
-        cc().setOpSettings(settings);
+            try {
+                ClientCursor::registerRunner(runner.get());
+                runner->setYieldPolicy(Runner::YIELD_AUTO);
+                safety.reset(new DeregisterEvenIfUnderlyingCodeThrows(runner.get()));
 
-        Lock::assertAtLeastReadLocked(ns);
-        try {
-            for (shared_ptr<Cursor> cursor = getOptimizedCursor( ns, query, BSONObj(), _countPlanPolicies );
-                 cursor->ok() ; cursor->advance() ) {
-                if ( cursor->currentMatches() && !cursor->getsetdup( cursor->currPK() ) ) {
-                    if ( skip > 0 ) {
-                        --skip;
+                Runner::RunnerState state;
+                while (Runner::RUNNER_ADVANCED == (state = runner->getNext(NULL, NULL))) {
+                    ++count;
+                }
+
+                // Emulate old behavior and return the count even if the runner was killed.  This
+                // happens when the underlying collection is dropped.
+                return count;
+            }
+            catch ( const DBException &e ) {
+                err = e.toString();
+                errCode = e.getCode();
+            } 
+            catch ( const std::exception &e ) {
+                err = e.what();
+                errCode = 0;
+            } 
+
+            // Historically we have returned zero in many count assertion cases - see SERVER-2291.
+            log() << "Count with ns: " << ns << " and query: " << query
+                  << " failed with exception: " << err << " code: " << errCode
+                  << endl;
+
+            return -2;
+        }
+        else {
+
+            shared_ptr<Cursor> cursor = getOptimizedCursor( ns, query, BSONObj(), _countPlanPolicies );
+            ClientCursorHolder ccPointer;
+            ElapsedTracker timeToStartYielding( 256, 20 );
+            try {
+                while( cursor->ok() ) {
+                    if ( !ccPointer ) {
+                        if ( timeToStartYielding.intervalHasElapsed() ) {
+                            // Lazily construct a ClientCursor, avoiding a performance regression when scanning a very
+                            // small number of documents.
+                            ccPointer.reset( new ClientCursor( QueryOption_NoCursorTimeout, cursor, ns ) );
+                        }
                     }
                     else {
                         ++count;
