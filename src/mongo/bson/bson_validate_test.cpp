6ddc26cd73daa0fe4c7801c14fc73c7092973103
Mathias Stearn <mathias@10gen.com>
2014-01-06 14:40:35 -0500

    SERVER-11903 Fix bug in bson_validate_test support code
    Not using the passed-in fieldname invalidated the ErrorIsInId test case.
    Luckily it still passes with this fixed.
    
    Conflicts:
    	src/mongo/bson/bson_validate_test.cpp

--- src/mongo/bson/bson_validate_test.cpp
+++ src/mongo/bson/bson_validate_test.cpp
@@ -22,6 +22,14 @@ namespace {
 
     using namespace mongo;
 
+    void appendInvalidStringElement(const char* fieldName, BufBuilder* bb) {
+        // like a BSONObj string, but without a NUL terminator.
+        bb->appendChar(String);
+        bb->appendStr(fieldName, /*withNUL*/true);
+        bb->appendNum(4);
+        bb->appendStr("asdf", /*withNUL*/false);
+    }
+
     TEST(BSONValidate, Basic) {
         BSONObj x;
         ASSERT_TRUE( x.valid() );
