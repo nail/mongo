0c0715c62f8a4a11bdc2ff7dfb661ec0ce9b158e
Dan Pasette <dan@10gen.com>
2014-01-14 19:06:59 -0500

    SERVER-2677 Allow hint to work with the count command.

--- src/mongo/s/commands_public.cpp
+++ src/mongo/s/commands_public.cpp
@@ -601,6 +601,10 @@ namespace mongo {
                     countCmdBuilder.append( "limit", limit );
                 }
 
+                if (cmdObj.hasField("hint")) {
+                    countCmdBuilder.append(cmdObj["hint"]);
+                }
+
                 if (cmdObj.hasField("$queryOptions")) {
                     countCmdBuilder.append(cmdObj["$queryOptions"]);
                 }
