0b9be1e0317fa1d53bab4c3f915c7e0b98c84e7d
Eliot Horowitz <eliot@10gen.com>
2014-02-03 07:07:39 -0500

    SERVER-12209: check _id for $fields at insert time
    Conflicts:
    	src/mongo/dbtests/pdfiletests.cpp

--- src/mongo/db/ops/insert.cpp
+++ src/mongo/db/ops/insert.cpp
@@ -94,6 +94,12 @@ namespace mongo {
                         return StatusWith<BSONObj>( ErrorCodes::BadValue,
                                                     "can't use an array for _id" );
                     }
+                    if ( e.type() == Object ) {
+                        BSONObj o = e.Obj();
+                        Status s = o.storageValidEmbedded();
+                        if ( !s.isOK() )
+                            return StatusWith<BSONObj>( s );
+                    }
                 }
 
             }
