367615fb1c47ac0fdc04bd646b5cc3e094cc2239
Hari Khalsa <hkhalsa@10gen.com>
2014-03-24 20:16:59 -0400

    SERVER-13336 count should always yield
    Revert "SERVER-12554 Lazily yield on count queries."
    
    This reverts commit 2850cc7253344c639bc9a01bc6dff3ffd2cca422.

--- src/mongo/db/ops/count.cpp
+++ src/mongo/db/ops/count.cpp
@@ -22,7 +22,6 @@
 #include "mongo/db/database.h"
 #include "mongo/db/structure/collection.h"
 #include "mongo/db/query/get_runner.h"
-#include "mongo/util/elapsed_tracker.h"
 
 namespace mongo {
 
@@ -82,20 +81,13 @@ namespace mongo {
         auto_ptr<Runner> runner(rawRunner);
         auto_ptr<DeregisterEvenIfUnderlyingCodeThrows> safety;
 
-        ElapsedTracker timeToStartYielding(128, 10);
         try {
             const ScopedRunnerRegistration safety(runner.get());
+            runner->setYieldPolicy(Runner::YIELD_AUTO);
 
             long long count = 0;
             Runner::RunnerState state;
             while (Runner::RUNNER_ADVANCED == (state = runner->getNext(NULL, NULL))) {
-
-                // Lazily yield, avoiding a performance regression when
-                // scanning a very small number of documents.
-                if (timeToStartYielding.intervalHasElapsed()) {
-                    runner->setYieldPolicy(Runner::YIELD_AUTO);
-                }
-
                 if (skip > 0) {
                     --skip;
                 }
